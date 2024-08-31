0b93cf0770cdb343b049a52d67f3601705414dcc
rohithzmoi <166651631+rohithzmoi@users.noreply.github.com>
2024-08-31 17:09:25 +0530

    Update dmr.cpp
    Signed-off-by: rohithzmoi <166651631+rohithzmoi@users.noreply.github.com>

--- dmr.cpp
+++ dmr.cpp
@@ -1,6 +1,6 @@
 /*
     Copyright (C) 2019-2021 Doug McLain
-
+    Modifications Copyright (C) 2024 Rohith Namboothiri
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
@@ -14,6 +14,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
+
 
 #include <iostream>
 #include <cstring>
