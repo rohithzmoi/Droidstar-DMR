220265669ad9c2aca08ad6682140e4508d05bfb9
rohithzmoi <166651631+rohithzmoi@users.noreply.github.com>
2024-09-07 14:44:45 +0530

    Update LogHandler.h
    added share functionality
    
    Signed-off-by: rohithzmoi <166651631+rohithzmoi@users.noreply.github.com>

--- LogHandler.h
+++ LogHandler.h
@@ -32,10 +32,11 @@ public:
     Q_INVOKABLE QJsonArray loadLog(const QString &fileName);
     Q_INVOKABLE bool clearLog(const QString &fileName);
     Q_INVOKABLE bool exportLogToCsv(const QString &fileName, const QJsonArray &logData);
-    Q_INVOKABLE QString getDSLogPath() const;  // Updated function name
-    Q_INVOKABLE QString getFriendlyPath(const QString &path) const; // Mark as Q_INVOKABLE
-
+    Q_INVOKABLE QString getDSLogPath() const; 
+    Q_INVOKABLE QString getFriendlyPath(const QString &path) const; 
     Q_INVOKABLE bool exportLogToAdif(const QString &fileName, const QJsonArray &logData);
+    Q_INVOKABLE void shareFile(const QString &filePath);
+    void shareFileDirectly(const QString &filePath);
 
 
 private:
